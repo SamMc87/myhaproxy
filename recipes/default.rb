7b29e51cba97c9cbca34032c1bd1e1c2045e4cee
SamMc87 <sam@technotrainer.com>
2018-03-25 23:39:17 -0600

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+	  'hostname' => 'cloud.public_hostname',
+	  'ipaddress' => 'cloud.public_ipv4',
+	  'port' => 80,
+	  'ssl_port' => 80
+	}]
+	
+include_recipe 'haproxy::manual'
