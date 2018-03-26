fa6b32ffa8982ec1fadd03d6b2d89a3a2631d02a
SamMc87 <sam@technotrainer.com>
2018-03-25 23:43:56 -0600

    Version 1.0.0

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,16 +4,20 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
-	  'hostname' => 'cloud.public_hostname',
-	  'ipaddress' => 'cloud.public_ipv4',
-	  'port' => 80,
-	  'ssl_port' => 80
-	},{
-	  'hostname' => 'cloud.public_hostname',
-	  'ipaddress' => 'cloud.public_ipv4',
-	  'port' => 80,
-	  'ssl_port' => 80
-	}]
-	
+all_web_nodes = search('node','role:web_server')
+
+members = []
+
+all_web_nodes.each do |web_node|
+  member = { 
+    'hostname' => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
+    'port' => 80,
+    'ssl_port' => 80
+  }
+  members.push(member)
+end
+
+node.default['haproxy']['members'] = members
+
 include_recipe 'haproxy::manual'
